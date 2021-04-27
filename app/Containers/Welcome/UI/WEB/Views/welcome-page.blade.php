2f90bc8850df618d77436fc044290adda528dbda
root <root@505215-ongrod.tmweb.ru>
2021-04-27 20:43:07 +0300

    working version apiatp 9

--- app/Containers/Welcome/UI/WEB/Views/welcome-page.blade.php
+++ app/Containers/Welcome/UI/WEB/Views/welcome-page.blade.php
@@ -72,7 +72,7 @@
 
     <div class="content">
 
-        <div class="title m-b-md">Apiato</div>
+        <div class="title m-b-md">Apiato test deploy</div>
 
         <div class="links">
             <a href="http://apiato.io/">Documentation</a>
