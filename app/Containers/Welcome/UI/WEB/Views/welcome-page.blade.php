a97ef0168eaef4a3576416bc5d5b1b90da84db5e
root <root@505215-ongrod.tmweb.ru>
2021-04-27 20:51:40 +0300

    test welcome message push v3

--- app/Containers/Welcome/UI/WEB/Views/welcome-page.blade.php
+++ app/Containers/Welcome/UI/WEB/Views/welcome-page.blade.php
@@ -72,7 +72,7 @@
 
     <div class="content">
 
-        <div class="title m-b-md">Apiato test deploy v2</div>
+        <div class="title m-b-md">Apiato test deploy v3</div>
 
         <div class="links">
             <a href="http://apiato.io/">Documentation</a>
